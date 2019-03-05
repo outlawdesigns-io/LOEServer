eabf03b6cef27395ca54f5b5043eb55d615ef70e
t3kj3wlz <outlawstar4761@gmail.com>
2019-03-05 10:33:00 -0600

    testing fs check

--- Processes/Scanners/Music/FsHealthScanner.php
+++ Processes/Scanners/Music/FsHealthScanner.php
@@ -11,10 +11,12 @@ class FsHealthScanner extends \MessageClient{
   const MSGNAME = "LOE_MUSIC_FS_CHECK";
   const MSGSUBJ = "Library of Everything File System Check";
 
-  public $files = array();
+  public $missing = array();
+  protected $_files = array();
 
   public function __construct(){
     $this->_scanForever(\LOE\LoeBase::WEBROOT . self::ROOTDIR)
+         ->_verifyDatabase();
   }
 
   protected function _scanForever($dir){
@@ -38,8 +40,28 @@ class FsHealthScanner extends \MessageClient{
   protected function _interpretFile($absolutePath){
     $fileInfo = pathinfo($absolutePath);
     if($fileInfo['extension'] == "mp3"){
-      $this->files[] = $absolutePath;
+      $this->_files[] = $absolutePath;
     }
     return $this;
+  }
+  protected function _verifyDatabase(){
+    foreach($this->_files as $file){
+      if(!$this->_recordExists($file)){
+        $this->missing[] = $absolutePath;
+      }
+    }
+    return $this;
+  }
+  protected function _recordExists($absolutePath){
+    $results = $GLOBALS['db']
+                ->database(\LOE\Song::DB)
+                ->table(\LOE\Song::TABLE)
+                ->select(\LOE\Song::PRIMARYKEY)
+                ->where("file_path","=","'" . $absolutePath . "'")
+                ->get();
+    if(!mysqli_num_rows($results)){
+      return false;
+    }
+    return true;
   }
 }
