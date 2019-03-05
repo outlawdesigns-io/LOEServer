213b546dd3fedef5933b31ed786351f745f396d6
t3kj3wlz <outlawstar4761@gmail.com>
2019-03-05 09:27:31 -0600

    building FS scanner

--- Processes/Scanners/Music/FsHealthScanner.php
+++ Processes/Scanners/Music/FsHealthScanner.php
@@ -1,11 +1,45 @@
 <?php namespace LOE\Music;
 
-require_once __DIR__ . '/../../Factory.php';
+require_once __DIR__ . '/../../../Factory.php';
 
 //todo scan LOE for all mp3s
 //todo foreach mp3 is this mp3 in the database?
 
 class FsHealthScanner extends \MessageClient{
 
-  public function __construct(){}
+  const ROOTDIR = '/LOE/Music/';
+  const MSGNAME = "LOE_MUSIC_FS_CHECK";
+  const MSGSUBJ = "Library of Everything File System Check";
+
+  public $files = array();
+
+  public function __construct(){
+    $this->_scanForever(\LOE\LoeBase::WEBROOT . self::ROOTDIR)
+  }
+
+  protected function _scanForever($dir){
+    $results = scandir($dir);
+    foreach($results as $result){
+      if($result == '.' || $result == '..'){
+        continue;
+      }else{
+        $tester = ($dir == \LOE\LoeBase::WEBROOT . self::ROOTDIR) ? $tester = $dir . $result : $tester = $dir . "/" . $result;
+      }
+      if(is_file($tester)){
+        $this->_interpretFile($tester);
+      }elseif(is_dir($tester)){
+        $this->_scanForever($tester);
+      }else{
+        continue;
+      }
+    }
+    return $this;
+  }
+  protected function _interpretFile($absolutePath){
+    $fileInfo = pathinfo($absolutePath);
+    if($fileInfo['extension'] == "mp3"){
+      $this->files[] = $absolutePath;
+    }
+    return $this;
+  }
 }
