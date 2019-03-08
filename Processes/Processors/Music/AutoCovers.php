417a3dc4178d4ccddcf9cc0b5f47db85132fbfff
t3kj3wlz <outlawstar4761@gmail.com>
2019-03-08 15:41:47 -0600

    testing extra slash paths

--- Processes/Processors/Music/AutoCovers.php
+++ Processes/Processors/Music/AutoCovers.php
@@ -51,6 +51,7 @@ class AutoCovers extends \LOE\FsScanner{
         $this->possibleCovers[] = $absolutePath;
       }
       if(!in_array($fileInfo['dirname'],$this->_hasCover) && !in_array($fileInfo['dirname'],$this->missing) && preg_match(self::OPENPATTERN,$fileInfo['dirname'])){
+        echo $absolutePath . "\n";
         $this->missing[] = $fileInfo['dirname'];
       }
       return $this;
