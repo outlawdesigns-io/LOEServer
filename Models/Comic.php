21dec39978a9396e635460a2798f860ea46f2799
t3kj3wlz <outlawstar4761@gmail.com>
2019-07-18 11:49:00 -0500

    adding comics and ComicVine Submodule

---
+++ Models/Comic.php
@@ -0,0 +1,36 @@
+<?php namespace LOE;
+
+require_once __DIR__ . '/../LoeBase.php';
+
+class Comic extends LoeBase{
+  const TABLE = 'comic';
+
+  public $UID;
+  public $issue_number;
+  public $issue_titile;
+  public $issue_cover_date;
+  public $series_title;
+  public $series_start_year;
+  public $series_end_year;
+  public $publisher;
+  public $story_arc;
+  public $issue_description;
+  public $series_description;
+  public $issue_type;
+  public $file_path;
+
+  public function __construct($UID = null){
+      parent::__construct(self::DB,self::TABLE,self::PRIMARYKEY,$UID);
+      $this->file_path = $this->_cleanFilePath($this->file_path);
+      $this->cover_path = $this->_cleanFilePath($this->cover_path);
+      $this->_cleanProperties();
+  }
+  public static function getAll(){
+      $data = array();
+      $ids = parent::getAll(self::DB,self::TABLE,self::PRIMARYKEY);
+      foreach($ids as $id){
+          $data[] = new self($id);
+      }
+      return $data;
+  }
+}
