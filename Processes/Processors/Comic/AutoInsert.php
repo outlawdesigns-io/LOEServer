ca4f0314a0eb780dcefe3e791bdc816f86047d68
t3kj3wlz <outlawstar4761@gmail.com>
2019-07-19 08:10:02 -0500

    typo

--- Processes/Processors/Comic/AutoInsert.php
+++ Processes/Processors/Comic/AutoInsert.php
@@ -19,7 +19,7 @@ class AutoInsert{
 
   public function __construct(){
     $this->scanner = new FsHealthScanner();
-    $this->_parse
+    $this->_parse();
     print_r($this->series);
   }
   protected function _parse(){
