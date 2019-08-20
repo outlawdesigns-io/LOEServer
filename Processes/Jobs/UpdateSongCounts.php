024d3e9696cdf33b7239f1cdaec3798078aba0f6
t3kj3wlz <outlawstar4761@gmail.com>
2019-08-20 12:30:47 -0500

    updating job

--- Processes/Jobs/UpdateSongCounts.php
+++ Processes/Jobs/UpdateSongCounts.php
@@ -4,7 +4,11 @@ require_once __DIR__ . '/../../Factory.php';
 require_once __DIR__ . '/AccountCredentials.php';
 
 try{
-  \LOE\Factory::updateSongCounts(ACCOUNT_USER,ACCOUNT_PASSWORD);
+  $processor = \LOE\Factory::updateSongCounts(ACCOUNT_USER,ACCOUNT_PASSWORD);
 }catch(\Exception $e){
   echo $e->getMessage() . "\n";
+}
+
+if(count($processor->exceptions)){
+  print_r($processor->exceptions);
 }
