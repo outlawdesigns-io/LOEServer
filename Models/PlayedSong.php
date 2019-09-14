1c3c4a5f86639a907e255fc18e517d3ea14bfe69
t3kj3wlz <outlawstar4761@gmail.com>
2019-09-14 07:56:29 -0500

    adding getAll to model

--- Models/PlayedSong.php
+++ Models/PlayedSong.php
@@ -33,4 +33,12 @@ class PlayedSong extends LoeBase{
   public static function countOf($key){
     return parent::countOf(self::TABLE,$key);
   }
+  public static function getAll(){
+      $data = array();
+      $ids = parent::getAll(self::DB,self::TABLE,self::PRIMARYKEY);
+      foreach($ids as $id){
+          $data[] = new self($id);
+      }
+      return $data;
+  }
 }
