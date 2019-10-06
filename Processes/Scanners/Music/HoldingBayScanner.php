b9f88ec050e034188339ec581dfb68d89fcc2636
t3kj3wlz <outlawstar4761@gmail.com>
2019-10-06 17:29:28 -0500

    fixing

--- Processes/Scanners/Music/HoldingBayScanner.php
+++ Processes/Scanners/Music/HoldingBayScanner.php
@@ -4,11 +4,7 @@ require_once __DIR__ . '/../../../Factory.php';
 
 ini_set('max_execution_time', 300);
 
-<<<<<<< HEAD:Processes/Scanners/HoldingBay/MusicScanner.php
-class MusicScanner extends FsScanner{
-=======
 class HoldingBayScanner extends \LOE\FsScanner{
->>>>>>> development:Processes/Scanners/Music/HoldingBayScanner.php
 
     const ROOTDIR = "/LOE/holding_bay/music";
 
@@ -23,23 +19,15 @@ class HoldingBayScanner extends \LOE\FsScanner{
     private $results = array();
 
     public function __construct(){
-<<<<<<< HEAD:Processes/Scanners/HoldingBay/MusicScanner.php
-        $this->_scanForever(\LOE\LoeBase::WEBROOT . self::ROOTDIR)
-=======
         $this->songCount = 0;
         $this->_scanForever(\LOE\Base::WEBROOT . self::ROOTDIR)
->>>>>>> development:Processes/Scanners/Music/HoldingBayScanner.php
             ->_getTags()
             ->_sortAlbums()
             ->_sortArtists();
     }
     protected function _interpretFile($absolutePath){
       $fileInfo = pathinfo($absolutePath);
-<<<<<<< HEAD:Processes/Scanners/HoldingBay/MusicScanner.php
-      $song = new Song();
-=======
       $song = \LOE\Factory::createModel(Song::TABLE);
->>>>>>> development:Processes/Scanners/Music/HoldingBayScanner.php
       if($fileInfo['extension'] == 'mp3'){
         $song->file_path = $song->cleanFilePath($absolutePath);
         $this->songs[] = $song;
