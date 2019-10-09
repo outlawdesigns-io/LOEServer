821d20e40bc175f4b798a601209a0419d0cb5e42
John J. Watson <jjwatson@ensafe.com>
2019-10-09 16:21:22 -0500

    updating bad references

--- Processes/Jobs/UpdateSongPlayHistory.php
+++ Processes/Jobs/UpdateSongPlayHistory.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../Factory.php';
 require_once __DIR__ . '/AccountCredentials.php';
 
 try{
-  $processor = \LOE\Factory::updatePlayHistory(\LOE\Song::TABLE,ACCOUNT_USER,ACCOUNT_PASSWORD);
+  $processor = \LOE\Factory::updatePlayHistory(\LOE\Music\Song::TABLE,ACCOUNT_USER,ACCOUNT_PASSWORD);
 }catch(\Exception $e){
   echo $e->getMessage() . "\n";
 }
