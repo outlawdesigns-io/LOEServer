789b0f47a2ffecf3eec58bb8847e29b3e92aaee3
John Watson <outlawstar4761@gmail.com>
2019-12-19 13:14:35 -0600

    abstracting

--- Models/PlayCountRun.php
+++ Models/PlayCountRun.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../Base.php';
 
 class PlayCountRun extends \LOE\Base{
 
-  const TABLE = 'Model';
+  const TABLE = 'PlayCountRun';
 
   public $UID;
   public $modelId;
@@ -16,19 +16,5 @@ class PlayCountRun extends \LOE\Base{
 
   public function __construct($UID = null){
     parent::__construct(self::DB,self::TABLE,self::PRIMARYKEY,$UID);
-  }
-  public static function getAll(){
-    $data = array();
-    $ids = parent::getAll(self::DB,self::TABLE,self::PRIMARYKEY);
-    foreach($ids as $id){
-      $data[] = new self($id);
-    }
-    return $data;
-  }
-  public static function count(){
-    return parent::count(self::TABLE);
-  }
-  public static function countOf($key){
-    return parent::countOf(self::TABLE,$key);
   }
 }
