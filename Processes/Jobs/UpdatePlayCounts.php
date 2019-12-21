9ab1937c544752868767d6b97c9cf18d6913976d
t3kj3wlz <outlawstar4761@gmail.com>
2019-12-21 14:52:47 -0600

    abstracting

---
+++ Processes/Jobs/UpdatePlayCounts.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../../Factory.php';
+require_once __DIR__ . '/AccountCredentials.php';
+
+$models = \LOE\Model::getAll();
+
+foreach($models as $model){
+  $startTime = microtime(true);
+  $run = \LOE\Factory::createModel('PlayCountRun');
+  $run->modelId = $model->UID;
+  $run->startTime = date("Y-m-d H:i:s");
+  try{
+    $processor = \LOE\Factory::updatePlayCounts($model,ACCOUNT_USER,ACCOUNT_PASSWORD);
+    $run->exceptionCount = count($processor->exceptions);
+    $run->processedCount = $processor->processedCount;
+    if(count($processor->exceptions)){
+      print_r($processor->exceptions);
+    }
+  }catch(\Exception $e){
+    echo $e->getMessage() . "\n";
+  }
+  $endTime = microtime(true);
+  $executionSeconds = $endTime - $startTime;
+  $run->endTime = date("Y-m-d H:i:s");
+  $run->runTime = $executionSeconds;
+  $run->create();
+}
