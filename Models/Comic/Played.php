557a0c6e28e65dbad2684d9f914b55b4a0b94fca
t3kj3wlz <outlawstar4761@gmail.com>
2019-12-21 15:41:14 -0600

    creating Played Models

---
+++ Models/Comic/Played.php
@@ -0,0 +1,30 @@
+<?php namespace LOE\Anime;
+
+require_once __DIR__ . '/../../Base.php';
+
+class Played extends \LOE\Base{
+
+  const TABLE = 'PlayedAnime';
+
+  public $UID;
+  public $comicId;
+  public $ipAddress;
+  public $playDate;
+
+  public function __construct($UID = null){
+    parent::__construct(self::DB,self::TABLE,self::PRIMARYKEY,$UID);
+  }
+  public static function recordExists($animeId,$playDate){
+    $results = $GLOBALS['db']
+        ->database(self::DB)
+        ->table(self::TABLE)
+        ->select(self::PRIMARYKEY)
+        ->where("comicId","=",$animeId)
+        ->andWhere("playDate","=","'" . $playDate . "'")
+        ->get();
+    if(!mysqli_num_rows($results)){
+      return false;
+    }
+    return true;
+  }
+}
---
+++ Models/Doc/Played.php
@@ -0,0 +1,30 @@
+<?php namespace LOE\Movie;
+
+require_once __DIR__ . '/../../Base.php';
+
+class Played extends \LOE\Base{
+
+  const TABLE = 'PlayedMovie';
+
+  public $UID;
+  public $docId;
+  public $ipAddress;
+  public $playDate;
+
+  public function __construct($UID = null){
+    parent::__construct(self::DB,self::TABLE,self::PRIMARYKEY,$UID);
+  }
+  public static function recordExists($docId,$playDate){
+    $results = $GLOBALS['db']
+        ->database(self::DB)
+        ->table(self::TABLE)
+        ->select(self::PRIMARYKEY)
+        ->where("docId","=",$docId)
+        ->andWhere("playDate","=","'" . $playDate . "'")
+        ->get();
+    if(!mysqli_num_rows($results)){
+      return false;
+    }
+    return true;
+  }
+}
