d3143bbbe2e2f93da7cf2d70fa22d81971018f84
John Watson <outlawstar4761@gmail.com>
2020-07-22 09:56:22 -0500

    testing

--- Processes/Scanners/Comic/HoldingBayScanner.php
+++ Processes/Scanners/Comic/HoldingBayScanner.php
@@ -4,12 +4,67 @@ require_once __DIR__ . '/../HoldingBayScanner.php';
 
 class HoldingBayScanner extends \LOE\HoldingBayScanner{
 
-    public $albums = array();
-    public $artists = array();
-    public $unknownAlbum = array();
-    public $unknownArtist = array();
+  const TITLEPAT = '/(([A-Z])(.*?))\(/';
+  const YEARPAT = '/\(([0-9]{4})\)/';
+  const ISSUEPAT = '/([0-9]{3})/';
 
-    public function __construct($model){
-      parent::__construct($model);
+  public $albums = array();
+  public $artists = array();
+  public $unknownAlbum = array();
+  public $unknownArtist = array();
+
+  public function __construct($model){
+    parent::__construct($model);
+  }
+  protected function _parseIssueName($fileName){
+    if(preg_match(self::TITLEPAT,$fileName,$matches)){
+      return $matches[1];
     }
+    return false;
+  }
+  protected function _parseIssueYear($fileName){
+    if(preg_match(self::YEARPAT,$fileName,$matches)){
+      return $matches[1];
+    }
+    return false;
+  }
+  protected function _parseIssueNumber($fileName){
+    if(preg_match(self::ISSUEPAT,$fileName,$matches)){
+      return $matches[0];
+    }
+    return false;
+  }
+  protected _build(){
+    for($i = 0; $i < count($this->targetModels); $i++){
+      $fileName = pathinfo($comic->file_path)['filename'];
+      $this->targetModels[$i]->UID = $i;
+      $this->targetModels[$i]->issue_number = $this->_parseIssueNumber($fileName);
+      $this->targetModels[$i]->issue_title = $this->_parseIssueName($fileName);
+    }
+    return $this;
+  }
 }
+
+/*
+foreach target Model
+  parse Title and Year
+  Search for Title.
+  Loop through results.
+  Use Year to confirm correct result.
+
+
+  public $UID;
+  public $issue_number;
+  public $issue_title;
+  public $issue_cover_date;
+  public $series_title;
+  public $series_start_year;
+  public $series_end_year;
+  public $publisher;
+  public $story_arc;
+  public $issue_description;
+  public $series_description;
+  public $issue_type;
+  public $file_path;
+
+*/
--- Processes/Scripts/TestFile.php
+++ Processes/Scripts/TestFile.php
@@ -1,9 +1,32 @@
 <?php namespace LOE;
 
 require_once __DIR__ . '/../../Factory.php';
+require_once __DIR__ . '/../../Libs/ComicVine/ComicVine.php';
+//$model = Factory::getModel(Comic\Comic::TABLE);
 
-$model = Factory::getModel(COmic\Comic::TABLE);
+//$scanner = Factory::createHoldingBayScanner($model);
 
-$scanner = Factory::createHoldingBayScanner($model);
+//print_r($scanner->targetModels);
 
-print_r($scanner->targetModels);
+$issueStrs = array("Batman - The Red Death 001","Teen Titans 012","Dark Nights - Metal 002");
+
+foreach($issueStrs as $str){
+  $results = \ComicVine::search($str);
+  foreach($results->results->volume as $volume){
+    echo $volume->name . "\n";
+  }
+}
+
+// $comic = Factory::createModel(Comic\Comic::TABLE);
+// $comic->issue_number = (float)$issueDetails->results->issue_number;
+// $comic->issue_title = (string)$issueDetails->results->name;
+// $comic->issue_cover_date = (string)$issueDetails->results->cover_date;
+// $comic->series_title = (string)$issueDetails->results->volume->name;
+// $comic->series_start_year = (string)$startYear;
+// $comic->series_end_year = "";
+// $comic->story_arc = (string)$issueDetails->results->story_arc_credits->story_arc->name;
+// $comic->issue_description = strip_tags((string)$issueDetails->results->description);
+// $comic->series_description = strip_tags((string)$seriesDescription);
+// $comic->issue_type = "";
+// $comic->publisher = $publisher;
+// $comic->file_path = $series->files[array_search($comic->issue_number,$series->issues)];
