9c371c44f58a2a65e20cbf68e5d74f3553001880
John Watson <outlawstar4761@gmail.com>
2020-07-31 08:48:16 -0500

    testing

--- Processes/Scripts/TestFile.php
+++ Processes/Scripts/TestFile.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../../Factory.php';
 require_once __DIR__ . '/../../Libs/ComicVine/ComicVine.php';
 
+
 $model = Factory::getModel(Comic\Comic::TABLE);
 $scanner = Factory::createHoldingBayScanner($model);
 
