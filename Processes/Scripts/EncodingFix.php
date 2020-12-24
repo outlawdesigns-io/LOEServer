ed13c259dfed00d94966c4c48aec49e5041ba102
John Watson <outlawstar4761@gmail.com>
2020-12-24 08:13:18 -0600

    updating a script

--- Processes/Scripts/EncodingFix.php
+++ Processes/Scripts/EncodingFix.php
@@ -3,7 +3,21 @@
 require_once __DIR__ . '/Factory.php';
 
 //$ids = file('uids');
-$ids = array(80454,80455,80456,80457,80458,80459,80460,80461,80462,80463);
+// $ids = array(80454,80455,80456,80457,80458,80459,80460,80461,80462,80463);
+function _getIds(){
+  $data = array();
+  $results = $GLOBALS['db']
+    ->database(Music\Song::DB)
+    ->table(Music\Song::TABLE)
+    ->select(Music\Song::PRIMARYKEY)
+    ->where('title','regexp',"'[^ -~]'")
+    ->get();
+  while($row = mysqli_fetch_assoc($results)){
+    $data[] = $row[Music\Song::PRIMARYKEY];
+  }
+  return $data;
+}
+
 foreach($ids as $id){
   $model = \LOE\Factory::createModel('Song',$id);
   $tags = $model->getMp3Tags();
